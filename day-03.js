e54af906ca09e24945ea1c5d9c344f2ba95ae7d2
bluesigor <87902135+bluesigor@users.noreply.github.com>
2021-09-21 21:39:34 +0300

    Update day-03.js

--- day-03.js
+++ day-03.js
@@ -1,23 +1,10 @@
 //   http://www.codewars.com/kata/571f832f07363d295d001ba8
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+function trueOrFalse(val) {
+  if (val <= false) return "false";
+  else if ((val > 0, val != true, val != undefined)) return "true";
+  else if (val != 0) return "false";
+}
 
 
 // https://www.codewars.com/kata/57202aefe8d6c514300001fd/train/javascript
