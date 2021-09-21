f5b147130254db160133146b5417cde06fd7da93
bluesigor <87902135+bluesigor@users.noreply.github.com>
2021-09-21 12:09:22 +0300

    Update day-02.js

--- day-02.js
+++ day-02.js
@@ -1,4 +1,5 @@
 //https://www.codewars.com/kata/571edd157e8954bab500032d/train/javascript
+
 var v1 = 50;
 (v2 = 100), (v3 = 150), (v4 = 200), (v5 = 2), (v6 = 250);
 function equal1() {
@@ -30,6 +31,7 @@ function equal5() {
 
 
 //https://www.codewars.com/kata/571edea4b625edcb51000d8e/train/javascript
+
 var a1 = "A",
   a2 = "a",
   b1 = "B",
@@ -69,4 +71,30 @@ function answer3() {
   return "yes";
 }
 
-//
+//https://www.codewars.com/kata/571effabb625ed9b0600107a/train/javascript
+
+function getLength(arr) {
+  //return length of arr
+  return arr.length;
+}
+function getFirst(arr) {
+  //return the first element of arr
+  return arr[0];
+}
+function getLast(arr) {
+  //return the last element of arr
+  return arr[arr.length - 1];
+}
+function pushElement(arr) {
+  const el = 4;
+  //push el to arr
+  arr.push(4);
+  return arr;
+}
+function popElement(arr) {
+  //pop an element from arr
+  arr.pop(2);
+  return arr;
+}
+
+//https://www.codewars.com/kata/571f1eb77e8954a812000837/train/javascript
