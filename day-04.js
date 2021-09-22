61c461ecfa8bfb7643c290cc1e05259f0e310527
bluesigor <87902135+bluesigor@users.noreply.github.com>
2021-09-22 23:09:55 +0300

    Create day-04.js

---
+++ day-04.js
@@ -0,0 +1,40 @@
+// https://www.codewars.com/kata/57216d4bcdd71175d6000560/train/javascript
+function padIt(str,n){
+ let i = 0;
+  while(i<n){
+    if(i%2 === 1) {
+      str = str + "*";
+    }else{
+      str = "*" + str;
+    }
+    i ++;
+  }
+  return str;
+}
+
+// https://www.codewars.com/kata/5721a78c283129e416000999/train/javascript
+
+
+
+
+
+
+
+//https://www.codewars.com/kata/5721c189cdd71194c1000b9b/train/javascript
+
+
+
+
+
+
+
+
+
+
+
+//https://www.codewars.com/kata/5722b3f0bd5583cf44001000
+
+
+
+
+
