d7c354405bd97affbebc9d9003351a350eb2550a
bluesigor <87902135+bluesigor@users.noreply.github.com>
2021-09-22 14:11:03 +0300

    Update day-03.js

--- day-03.js
+++ day-03.js
@@ -9,15 +9,9 @@ function trueOrFalse(val) {
 
 
 // https://www.codewars.com/kata/57202aefe8d6c514300001fd/train/javascript
-function saleHotdogs(n) {
-  if (n < 5) {
-    return n * 100;
-  } else if (n >= 5 && n < 10) {
-    return n * 95;
-  } else n <= 10;
-  {
-    return n * 90;
-  }
+
+function saleHotdogs(n){
+  return n < 5 ? n*100 : n >= 5 && n < 10 ? n*95: n >= 10 ? n*90:"saleHotdogs"; 
 }
 
 // http://www.codewars.com/kata/572059afc2f4612825000d8a
