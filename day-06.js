8a76da0d27cca7e899724e4da74cec8d6ff30f28
bluesigor <87902135+bluesigor@users.noreply.github.com>
2021-09-24 00:37:57 +0300

    Update day-06.js

--- day-06.js
+++ day-06.js
@@ -10,4 +10,8 @@ function hero(bullets, dragons) {
   return bullets >= dragons * 2;
 }
 
-//
+// https://www.codewars.com/kata/59441520102eaa25260000bf/train/javascript
+
+function unusualFive() {
+  return "metre".length;
+}
