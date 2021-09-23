6447a0bd70abeb21ad9e91bedb99c2c4c7142c25
bluesigor <87902135+bluesigor@users.noreply.github.com>
2021-09-24 00:20:57 +0300

    Update day-06.js

--- day-06.js
+++ day-06.js
@@ -1,3 +1,9 @@
+// https://www.codewars.com/kata/maximum-multiple/train/javascript
+
+function maxMultiple(divisor, bound) {
+  return bound - (bound % divisor);
+}
+
 // https://www.codewars.com/kata/is-he-gonna-survive/train/javascript
 
 function hero(bullets, dragons) {
