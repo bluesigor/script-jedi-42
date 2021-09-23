a8185fbf8c20081d35ac845bebb06e3086985f49
bluesigor <87902135+bluesigor@users.noreply.github.com>
2021-09-23 13:30:21 +0300

    Update day-04.js

--- day-04.js
+++ day-04.js
@@ -49,8 +49,8 @@ function grabDoll(dolls) {
 function giveMeFive(obj) {
   const five = [];
   for (let key in obj) {
-    if (key.length == 5) five.push(key);
-    if (obj[key].length == 5) five.push(obj[key]);
+    if (key.length === 5) five.push(key);
+    if (obj[key].length === 5) five.push(obj[key]);
   }
   return five;
 }
