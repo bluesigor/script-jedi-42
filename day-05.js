62d5f4855fd321efe67893cc7dbc930af9be1d2d
bluesigor <87902135+bluesigor@users.noreply.github.com>
2021-09-23 19:07:59 +0300

    Create day-05.js

---
+++ day-05.js
@@ -0,0 +1,21 @@
+//https://www.codewars.com/kata/is-this-my-tail/train/javascript
+
+function automorphic(n) {
+  const num = n + "";
+  const square = n ** 2 + "";
+  if (square.endsWith(num)) return "Automorphic";
+  else return "Not!!";
+}
+
+//https://www.codewars.com/kata/56f6ad906b88de513f000d96/train/javascript
+
+function bonusTime(salary, bonus) {
+  if (bonus === true) {
+    return "\u00A3" + salary * 10;
+  } else {
+    return "\u00A3" + salary;
+  }
+}
+
+//https://www.codewars.com/kata/5a58d889880385c2f40000aa
+
