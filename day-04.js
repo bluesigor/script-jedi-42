48f337c84e798b0eb40c7d696cd149f85c7a584e
bluesigor <87902135+bluesigor@users.noreply.github.com>
2021-09-23 11:07:06 +0300

    Update day-04.js

--- day-04.js
+++ day-04.js
@@ -1,24 +1,32 @@
 // https://www.codewars.com/kata/57216d4bcdd71175d6000560/train/javascript
-function padIt(str,n){
- let i = 0;
-  while(i<n){
-    if(i%2 === 1) {
+
+function padIt(str, n) {
+  let i = 0;
+  while (i < n) {
+    if (i % 2 === 1) {
       str = str + "*";
-    }else{
+    } else {
       str = "*" + str;
     }
-    i ++;
+    i++;
   }
   return str;
 }
 
 // https://www.codewars.com/kata/5721a78c283129e416000999/train/javascript
 
-
-
-
-
-
+function pickIt(arr) {
+  const odd = [],
+    even = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) {
+      even.push(arr[i]);
+    } else {
+      odd.push(arr[i]);
+    }
+  }
+  return [odd, even];
+}
 
 //https://www.codewars.com/kata/5721c189cdd71194c1000b9b/train/javascript
 
