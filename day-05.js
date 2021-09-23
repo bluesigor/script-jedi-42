f9b1bb38f051e3224e3a7505f08ff6ebf8d6a5f4
bluesigor <87902135+bluesigor@users.noreply.github.com>
2021-09-23 19:39:48 +0300

    Update day-05.js

--- day-05.js
+++ day-05.js
@@ -1,10 +1,7 @@
 //https://www.codewars.com/kata/is-this-my-tail/train/javascript
 
-function automorphic(n) {
-  const num = n + "";
-  const square = n ** 2 + "";
-  if (square.endsWith(num)) return "Automorphic";
-  else return "Not!!";
+function correctTail(body, tail) {
+  return body.endsWith(tail);
 }
 
 //https://www.codewars.com/kata/56f6ad906b88de513f000d96/train/javascript
@@ -19,3 +16,9 @@ function bonusTime(salary, bonus) {
 
 //https://www.codewars.com/kata/5a58d889880385c2f40000aa
 
+function automorphic(n) {
+  const num = n + "";
+  const square = n ** 2 + "";
+  if (square.endsWith(num)) return "Automorphic";
+  else return "Not!!";
+}
