82727769af3791e27a28cc20767966c7ae25061f
bluesigor <87902135+bluesigor@users.noreply.github.com>
2021-09-27 12:22:22 +0300

    Update day-08.js

--- day-08.js
+++ day-08.js
@@ -10,6 +10,27 @@ function whatNumberIsIt(n) {
   if (Number.isNaN(n)) return input + "Number.NaN";
   if (Number.isSafeInteger(n)) return input + n;
 }
-//  https://www.codewars.com/kata/5722fd3ab7162a3a4500031f/solutions/javascript
+//  https://www.codewars.com/kata/57238ceaef9008adc7000603/train/javascript
+
+function colorOf(r, g, b) {
+  let red = r.toString(16);
+  let green = g.toString(16);
+  let blue = b.toString(16);
+
+  if (r < 16) red = "0" + red;
+  if (g < 16) green = "0" + green;
+  if (b < 16) blue = "0" + blue;
+  return "#" + red + green + blue;
+}
+
+// https://www.codewars.com/kata/57256064856584bc47000611
+
+function howManySmaller(arr, n) {
+  let result = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].toFixed(2) < n) result++;
+  }
+  return result;
+}
 
 
