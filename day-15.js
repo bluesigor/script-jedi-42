f7c703911105733d92083db886ed7e23b1a8eb7a
bluesigor <87902135+bluesigor@users.noreply.github.com>
2021-10-04 17:52:15 +0300

    Update day-15.js

--- day-15.js
+++ day-15.js
@@ -6,4 +6,23 @@ function shuffleIt(arr, ...swap) {
   return arr;
 }
 
-//
+//https://www.codewars.com/kata/5fd8aa5743b49e0012d43e50/
+
+
+//https://www.codewars.com/kata/572af273a3af3836660014a1/train/javascript
+
+function infiniteLoop(arr, d, n) {
+  for (let i = 1; i <= n; i++) {
+    if (d === "left") {
+      arr[2].push(arr[0].shift());
+      arr[1].push(arr[2].shift());
+      arr[0].push(arr[1].shift());
+    }
+    if (d === "right") {
+      arr[0].unshift(arr[2].pop());
+      arr[1].unshift(arr[0].pop());
+      arr[2].unshift(arr[1].pop());
+    }
+  }
+  return arr;
+}
