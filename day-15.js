99e30b18116c97e56623096c9de4693da1b969fb
bluesigor <87902135+bluesigor@users.noreply.github.com>
2021-10-04 11:44:27 +0300

    Update day-15.js

--- day-15.js
+++ day-15.js
@@ -1,1 +1,9 @@
+// https://www.codewars.com/kata/572ab0cfa3af384df7000ff8/train/javascript
 
+function shuffleIt(arr, ...swap) {
+  for (const index of swap)
+    [arr[index[0]], arr[index[1]]] = [arr[index[1]], arr[index[0]]];
+  return arr;
+}
+
+//
