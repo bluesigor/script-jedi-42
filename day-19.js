157008622426984bb61c6b75d4fe909d81237554
bluesigor <87902135+bluesigor@users.noreply.github.com>
2021-10-08 14:35:26 +0300

    Create day-19.js

---
+++ day-19.js
@@ -0,0 +1,21 @@
+// https://www.codewars.com/kata/5735e39313c205fe39001173/train/javascript
+
+function countAnimals(animals,count){
+  const result = []
+  for(let i = 0; i < count.length; i++){
+  let match = (animals.match(new RegExp(count[i],"g"))||[]).length;
+  result.push(match)
+    }
+  return result
+}
+
+// https://www.codewars.com/kata/573975d3ac3eec695b0013e0
+
+function findSimilarity(str, word) {
+  const regularstr = word[0] + word.slice(1, -1).replace(/./g, ".") + word.slice(-1);
+  const reg = new RegExp("^" + regularstr + "$");
+  return str
+    .split(" ")
+    .filter((v) => reg.test(v))
+    .join(" ");
+}
