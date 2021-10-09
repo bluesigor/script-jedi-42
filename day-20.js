e8fa5307cd9689b4988267cdda48bc67797e90ef
bluesigor <87902135+bluesigor@users.noreply.github.com>
2021-10-09 09:44:08 +0300

    Create day-20.js

---
+++ day-20.js
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/573bca07dffc1aa693000139/train/javascript
+
+const regex= /^(-?)9.*0000$/;
+
+// https://www.codewars.com/kata/573d11c48b97c0ad970002d4/train/javascript
+
+const regex=/(http|https):\/\/[a-z\d.]+\.(net|com)/gi ;
