1d7b61f65b56d0d6fd296914093468d1f77aefec
bluesigor <87902135+bluesigor@users.noreply.github.com>
2021-10-16 11:40:19 +0300

    Create day-27.js

---
+++ day-27.js
@@ -0,0 +1,22 @@
+// https://www.codewars.com/kata/56b71b1dbd06e6d88100092a/train/javascript
+
+function buildFun(n) {
+  const res = [];
+  for (let i = 0; i < n; i++) {
+    res.push(function () {
+      return i;
+    });
+  }
+  return res;
+}
+
+// https://www.codewars.com/kata/shifty-closures/train/javascript
+
+const greet_abe = function () {
+  const name = "Abe";
+  return "Hello, " + name + "!";
+};
+const greet_ben = function () {
+  const name = "Ben";
+  return "Hello, " + name + "!";
+};
