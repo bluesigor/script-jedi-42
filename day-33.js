da3c86916f5eeaca5809274737265c2c0a279923
bluesigor <87902135+bluesigor@users.noreply.github.com>
2021-10-23 10:10:11 +0300

    Update day-33.js

--- day-33.js
+++ day-33.js
@@ -1,1 +1,12 @@
+// https://www.codewars.com/kata/array-number-reduce/train/javascript
 
+
+
+// https://www.codewars.com/kata/52ed80dfe7bf9ae61d000056/train/javascript
+
+Object.deepFreeze = function (object) {
+  Object.freeze(object);
+  for (let i in object) {
+    Object.deepFreeze(object[i]);
+  }
+};
