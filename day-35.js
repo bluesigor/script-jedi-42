1f87ebe7743ad18c267e28516cfa787cd27bdffa
bluesigor <87902135+bluesigor@users.noreply.github.com>
2021-10-24 09:52:46 +0300

    Create day-35.js

---
+++ day-35.js
@@ -0,0 +1,15 @@
+// https://www.codewars.com/kata/557e508a47c7e9adf9000062/train/javascript
+
+Object.prototype.description = "TODO: GIVE ME A DESCRIPTION"
+ 
+// https://www.codewars.com/kata/513e1e47c600c93cef000001/train/javascript
+
+class Animal {
+  constructor(name, type) {
+    this.name = name;
+    this.type = type;
+  }
+  toString() {
+    return `${this.name} is a ${this.type}`;
+  }
+}
